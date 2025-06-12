5f0181b7577de2f2c88288468a2e22b925b7831c
Tsyren Nimaev <cnimaev@gmail.com>
2025-06-12 21:37:01 +0300

    feat: create basic SearchBar component with geocoding query

---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,62 @@
+import { useGetCoordinatesByCityNameQuery } from '@/api/geocodingApi';
+import React, { useState } from 'react';
+import type { FetchBaseQueryError } from '@reduxjs/toolkit/query';
+import type { SerializedError } from '@reduxjs/toolkit';
+
+interface OpenCageError {
+  status?: {
+    message?: string;
+  };
+}
+
+const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  //пока не использую данные, просто запускаю запрос при сабмите
+  const { refetch, isFetching, error } = useGetCoordinatesByCityNameQuery(
+    searchTerm,
+    {
+      skip: true /*запрос не запускается автоматически*/,
+    }
+  );
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (searchTerm.trim()) {
+      refetch(); //запускаем запрос в ручеую
+    }
+  };
+
+  const getErrorMessage = (error: FetchBaseQueryError | SerializedError) => {
+    if ('data' in error) {
+      const opencageError = error.data as OpenCageError;
+      if (opencageError?.status?.message) {
+        return opencageError.status.message;
+      }
+    }
+
+    if ('message' in error && typeof error.message === 'string') {
+      return error.message;
+    }
+
+    return 'Failed to featch location';
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type='text'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        placeholder='Enter city name'
+        disabled={isFetching}
+      />
+      <button type='submit' disabled={isFetching}>
+        {isFetching ? 'Searching...' : 'Search'}
+      </button>
+      {error && <div>Error: {getErrorMessage(error)}</div>}
+    </form>
+  );
+};
+
+export default SearchBar;
