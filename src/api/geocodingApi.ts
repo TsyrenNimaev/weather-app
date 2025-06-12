614f084578203a619204ae080776940c4ed82e47
Tsyren Nimaev <cnimaev@gmail.com>
2025-06-12 20:53:59 +0300

    feat: setup RTK Query for Geocoding API

---
+++ src/api/geocodingApi.ts
@@ -0,0 +1,31 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import type { GeocodingResult } from './weatherTypes';
+
+interface GeocodingResponse {
+  results: GeocodingResult[];
+}
+
+export const geocodingApi = createApi({
+  reducerPath: 'geocodingApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_GEOCODING_API_URL,
+  }),
+  endpoints: (builder) => ({
+    getCoordinatesByCityName: builder.query<GeocodingResult[], string>({
+      query: (cityName) => ({
+        url: '/json',
+        params: {
+          q: cityName,
+          key: import.meta.env.VITE_GEOCODING_API_KEY,
+          limit: 5,
+          no_annotations: 1,
+          language: 'en',
+        },
+      }),
+      transformResponse: (response: GeocodingResponse) => response.results,
+      // Обработка ошибок
+    }),
+  }),
+});
+
+export const { useGetCoordinatesByCityNameQuery } = geocodingApi;
